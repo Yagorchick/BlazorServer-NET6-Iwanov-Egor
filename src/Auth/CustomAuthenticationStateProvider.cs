dd6fc995bc882c49637ffef998b50b974170ce1f
Egor <chrissuco537@gmail.com>
2024-11-28 18:19:05 +0300

    Files

---
+++ src/Auth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,86 @@
+﻿using BlazorServer_NET6_Iwanov_Egor.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly ProtectedSessionStorage _sessionStorage;
+    private readonly ProtectedLocalStorage _localStorage;
+    private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public CustomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage, ProtectedLocalStorage localStorage)
+    {
+        _sessionStorage = sessionStorage;
+        _localStorage = localStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        try
+        {
+            var userSessionLocalStorageResult = await _localStorage.GetAsync<UserSession>("UserSession");
+            if (userSessionLocalStorageResult.Success && userSessionLocalStorageResult.Value != null)
+            {
+                var userSession = userSessionLocalStorageResult.Value;
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Sid, userSession.Id),
+                    new Claim(ClaimTypes.Name, userSession.FirstName),
+                    new Claim(ClaimTypes.Email, userSession.Email),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }, "CustomAuth"));
+
+                return new AuthenticationState(claimsPrincipal);
+            }
+
+            var userSessionSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+            var userSessionSession = userSessionSessionStorageResult.Success ? userSessionSessionStorageResult.Value : null;
+            if (userSessionSession == null)
+            {
+                return new AuthenticationState(_anonymous);
+            }
+
+            var claimsPrincipalSession = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Sid, userSessionSession.Id),
+                new Claim(ClaimTypes.Name, userSessionSession.FirstName),
+                new Claim(ClaimTypes.Email, userSessionSession.Email),
+                new Claim(ClaimTypes.Role, userSessionSession.Role)
+            }, "CustomAuth"));
+
+            return new AuthenticationState(claimsPrincipalSession);
+        }
+        catch
+        {
+            return new AuthenticationState(_anonymous);
+        }
+    }
+
+    public async Task UpdateAuthenticationStateAsync(UserSession userSession)
+    {
+        ClaimsPrincipal claimsPrincipal;
+
+        if (userSession != null)
+        {
+            await _localStorage.SetAsync("UserSession", userSession);
+            await _sessionStorage.SetAsync("UserSession", userSession);
+
+            claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Sid, userSession.Id),
+                new Claim(ClaimTypes.Name, userSession.FirstName),
+                new Claim(ClaimTypes.Email, userSession.Email),
+                new Claim(ClaimTypes.Role, userSession.Role)
+            }));
+        }
+        else
+        {
+            await _localStorage.DeleteAsync("UserSession");
+            await _sessionStorage.DeleteAsync("UserSession");
+            claimsPrincipal = _anonymous;
+        }
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,73 @@
+using BlazorServer_NET6_Iwanov_Egor.Auth;
+using BlazorServer_NET6_Iwanov_Egor.Data;
+using BlazorServer_NET6_Iwanov_Egor.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServer_NET6_Iwanov_Egor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("Server=sql.bsite.net\\MSSQL2016;Database=yagorchick_;User Id=yagorchick_;Password=23042003;TrustServerCertificate=true;");
+            Console.WriteLine($"Connection String: {connectionString}");
+
+            builder.Services.AddDbContext<YagorchickContext>(options =>
+                options.UseSqlServer("Server=sql.bsite.net\\MSSQL2016;Database=yagorchick_;User Id=yagorchick_;Password=23042003;TrustServerCertificate=true;"));
+
+            builder.Services.AddAuthenticationCore();
+            //builder.Services.AddRazorComponents()
+                //.AddInteractiveServerComponents();
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<ProtectedSessionStorage>();
+            builder.Services.AddScoped<ProtectedLocalStorage>();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            //app.UseAntiforgery();
+
+            //app.MapRazorComponents<App>()
+                //.AddInteractiveServerRenderMode();
+
+            app.MapRazorPages();
+            // app.MapBlazorHub();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Program>();
+            });
+
+    }
+}
